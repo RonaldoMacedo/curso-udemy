05a8e544610a761e708d544d2e12d5db3eaa0887
RonaldoMacedo <ronni29_100@hotmail.com>
2024-06-11 15:07:50 -0300

    Aula 70: exercicio 1

---
+++ src/entites/Rectangle.java
@@ -0,0 +1,19 @@
+package entites;
+
+public class Rectangle {
+	
+	public double width, height;
+	
+	public double area() {
+		return width * height;
+	}
+	
+	public double perimeter() {
+		return width + height + width + height;
+	}
+	
+	public double diagonal() {
+		return Math.sqrt((width * width) + (height * height));
+	}
+
+}
