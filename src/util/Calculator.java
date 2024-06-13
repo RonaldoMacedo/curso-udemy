868917b393fd60d3dcdea8f2ebac19cf9252a95b
RonaldoMacedo <ronni29_100@hotmail.com>
2024-06-13 14:59:26 -0300

    Aula 71: membros estáticos versão 3 - versão correta

--- src/util/Calculator.java
+++ src/util/Calculator.java
@@ -2,13 +2,13 @@ package util;
 
 public class Calculator {
 	
-	public final double PI = 3.14159; // constantes com letra maiúsculas
+	public static final double PI = 3.14159; // constantes com letra maiúsculas
 	
-	public double volume(double radius) {
+	public static double volume(double radius) {
 		return 4.0 * PI * radius * radius * radius / 3.0;
 	}
 
-	public double circumference(double radius) {
+	public static double circumference(double radius) {
 		return 2 * PI * radius;
 	}
 
