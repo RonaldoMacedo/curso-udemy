eb04e0713e5d17a55373aa5982c7824309ffcf5b
RonaldoMacedo <ronni29_100@hotmail.com>
2024-06-21 11:55:42 -0300

    Aula 93: correção exercício 2

--- src/application/Program12.java
+++ src/application/Program12.java
@@ -8,51 +8,57 @@ import entites.Pessoa;
 public class Program12 {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Quantas pessoas serão lidas?");
 		int n = sc.nextInt();
-		
+
 		Pessoa[] vect = new Pessoa[n];
-		
-		int menor = 0;
-		
-		for(int i = 0; i < vect.length; i++) {
-			System.out.println("Dados da " + (i+1) + "° pessoa:");
+
+		for (int i = 0; i < vect.length; i++) {
+			System.out.println("Dados da " + (i + 1) + "° pessoa:");
 			System.out.print("Nome: ");
 			sc.nextLine();
 			String nome = sc.nextLine();
 			System.out.print("Idade: ");
 			int idade = sc.nextInt();
-			if(idade > 16) {
-				menor += 1;
-			}
 			System.out.print("Altura: ");
 			double altura = sc.nextDouble();
-			
+
 			vect[i] = new Pessoa(nome, idade, altura);
 		}
-		
+
 		double soma = 0.0;
 		double media = 0.0;
-		
-		for(int i = 0; i < vect.length; i++) {
+
+		for (int i = 0; i < vect.length; i++) {
 			soma += vect[i].getAltura();
-			media = soma / (i+1);
+			media = soma / (i + 1);
 		}
-		
+
 		System.out.println();
 		System.out.printf("Altura média: %.2f%n", media);
-		System.out.println("Pessoas com menos de 16 anos: " + menor);
-		
-		for(int i = 0; i < vect.length; i++) {
-			if(vect[i].getIdade() < 16) {
+
+		int menor = 0;
+
+		for (int i = 0; i < vect.length; i++) {
+			if (vect[i].getIdade() < 16) {
+				menor += 1;
+			}
+		}
+
+		double percent = menor * 100.0 / n;
+
+		System.out.println("Pessoas com menos de 16 anos: " + percent + "%");
+
+		for (int i = 0; i < vect.length; i++) {
+			if (vect[i].getIdade() < 16) {
 				System.out.println(vect[i]);
 			}
 		}
-		
+
 		sc.close();
 
 	}
