def14ca2500c1ae8ccc904c0abd718390f1053ef
RonaldoMacedo <ronni29_100@hotmail.com>
2024-09-17 16:10:06 -0300

    Aula 27: entrada de dados - parte 2

--- src/application/Program26.java
+++ src/application/Program26.java
@@ -1,43 +1,40 @@
 package application;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Program26 {
 
 	public static void main(String[] args) {
-		
-		Locale.setDefault(Locale.US);
+
 		Scanner sc = new Scanner(System.in);
-		
-		String x;
-		x = sc.next();
-		System.out.println("Você digitou " + x);
-		
-		int y;
-		y = sc.nextInt();
-		System.out.println("Você digitou " + y);
-		
-		double z;
-		z = sc.nextDouble();
-		System.out.println("Você digitou " + z);
-		
-		char a;
-		a = sc.next().charAt(0);
-		System.out.println("Você digitou " + a);
-		
-		String X;
-		int Y;
-		double Z;
-		X = sc.next();
-		Y = sc.nextInt();
-		Z = sc.nextDouble();
-		
-		System.out.println("Dados digitados: ");
-		System.out.println(X);
-		System.out.println(Y);
-		System.out.println(Z);
-		
+
+		String s1, s2, s3;
+
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+
+		System.out.println("Dados digitados:");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
+		System.out.println();
+		int x;
+		String s4, s5, s6;
+
+		x = sc.nextInt();
+		sc.nextLine(); //limpando o buffer de leitura - consumindo quebra de linha pendente
+		s4 = sc.nextLine();
+		s5 = sc.nextLine();
+		s6 = sc.nextLine();
+
+		System.out.println("Dados digitados:");
+		System.out.println(x);
+		System.out.println(s4);
+		System.out.println(s5);
+		System.out.println(s6);
+
 		sc.close();
 
 	}
