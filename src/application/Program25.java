5205341356fadfae2648f2ca51ea12fb1389dbef
RonaldoMacedo <ronni29_100@hotmail.com>
2024-09-17 15:39:23 -0300

    Aula 25: processamento de dados e casting

---
+++ src/application/Program25.java
@@ -0,0 +1,61 @@
+package application;
+
+public class Program25 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Exemplo 1:");
+		int x, y;
+		x = 5;
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println();
+		System.out.println("Exemplo 2:");
+		int X;
+		double Y;
+		
+		X = 5;
+		Y = 2 * X;
+		
+		System.out.println(X);
+		System.out.println(Y);
+		
+		System.out.println();
+		System.out.println("Exemplo 3:");
+		double b, B, h, area;
+		
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		area = (b + B) / 2.0 * h;
+		
+		System.out.println(area);
+		
+		System.out.println();
+		System.out.println("Exemplo 4:");
+		int a, A;
+		double resultado;
+		
+		a = 5;
+		A = 2;
+		resultado = (double) a/A; //casting
+		
+		System.out.println(resultado);
+		
+		System.out.println();
+		System.out.println("Exemplo 5:");
+		
+		double r;
+		int s;
+		
+		r = 5.0;
+		s = (int)r;
+		
+		System.out.println(s);
+
+	}
+
+}
