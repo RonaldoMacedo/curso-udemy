e8e4075f73579553da1c202e674515ea1e6081bf
RonaldoMacedo <ronni29_100@hotmail.com>
2024-10-16 14:37:44 -0300

    Aula 39: switch-case sintaxe

---
+++ src/application/Program45.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program45 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		String dia;
+		
+		switch(x) {
+		case 1:
+			dia = "domingo";
+			break;
+		case 2:
+			dia = "segunda-feira";
+			break;
+		case 3:
+			dia = "terça-feira";
+			break;
+		case 4:
+			dia = "quarta-feira";
+			break;
+		case 5:
+			dia = "quinta-feira";
+			break;
+		case 6:
+			dia = "sexta-feira";
+			break;
+		case 7:
+			dia = "sábado";
+			break;
+		default:
+			dia = "valor inválido";
+			break;
+		}
+		
+		System.out.println("Dia da semana: " + dia);
+		
+		sc.close();
+
+	}
+
+}
