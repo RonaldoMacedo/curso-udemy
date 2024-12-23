4cbf5aefd81c2904c94e34b67341cda2ce5a5170
RonaldoMacedo <ronni29_100@hotmail.com>
2024-12-23 16:09:16 -0300

    Aula 168: solução com interface

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -10,24 +10,23 @@ public class RentalService {
 	private Double pricePerDay;
 	private Double pricePerHour;
 	
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
 	}
 	
 	public void processInvoice(CarRental carRental) {
-		
 		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
 		double hours = minutes / 60.0;
 		
 		double basicPayment;
 		if(hours <= 12.0) {
-			basicPayment = pricePerHour * Math.ceil(hours);
+			basicPayment = Math.ceil(hours) * pricePerHour;
 		}else {
-			basicPayment = pricePerDay * Math.ceil(hours / 24);
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
 		}
 		
 		double tax = taxService.tax(basicPayment);
